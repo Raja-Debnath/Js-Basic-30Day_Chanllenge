c2439083dcee4a557519499705b8dd7b79a20609
Raja Debnath <piratesking2110@gmail.com>
2024-07-21 18:49:29 +0530

    started func

---
+++ func.js
@@ -0,0 +1,41 @@
+// task 1 
+
+// function EvenOdd(a) {
+//     return a%2==0 ? "even":"odd"
+// }
+// console.log(EvenOdd(21));
+
+// task 2
+
+// function square(a) {
+//     return a*a
+// }
+// console.log(square(7));
+
+// task 3 
+
+// function maxNum(a,b) {
+//     return a<b?b:a
+    
+// }
+// console.log(maxNum(2,1));
+
+// task 4
+
+// function concat(a,b) {
+//     return a+b
+// }
+// console.log(concat("raja ","debnath"))
+
+// task 5 
+
+// const add =(a,b)=> a+b
+// console.log(add(5,8));
+
+// task 6 
+
+// const check = (a)=>{
+//     return a.includes("$")?true : false
+// }
+// console.log(check("raja$aa"));
+
--- loops.js
+++ loops.js
@@ -89,3 +89,21 @@
 // }else{console.log(index);}
 // }
 
+
+// pattern 
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// for (let i = 5; i >= 1; i--) {
+//     let stars = ""
+//     for (let j = 1; j <= i; j++) {
+//         stars+="*"
+//         // console.log("a");
+//     }
+//     console.log(stars);
+// }
+
