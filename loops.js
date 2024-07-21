972b86c8bc2534e3b61a1fc84f57c51d3bc133fc
Raja Debnath <piratesking2110@gmail.com>
2024-07-21 06:26:56 +0530

    loops done

---
+++ loops.js
@@ -0,0 +1,91 @@
+// task 1 , print 1 - 10 
+
+// for (let index = 1; index <= 10 ; index++) {
+//     console.log(index)
+// }
+
+// task 2  , 5 ka tabel
+
+
+// for (let index = 1; index <= 10 ; index++) {
+//     console.log(index*5)
+// }
+
+// task 3 sum of 1 to 10 
+
+// let index = 0
+// let total = 0
+// while (index<=10) {
+    
+//     total=total+index
+//     index++
+//     console.log(index,":",total);
+// }
+
+// task 4 print 10 to 1 using while 
+
+
+// let index = 10
+
+// while (!index==0) {
+//     console.log(index);
+//     index--
+// }
+
+
+// task5 
+// let index = 1
+// do {
+//     console.log(index);
+//     index++
+// } while (index<=5);
+
+// task 6 factorial 
+
+// let index = 1
+// let total =1
+// let fact = 20 
+// do {
+//      total = total*index
+//     console.log(total);
+//     index++
+// } while (index<=fact);
+
+// task 7 
+// for (let index = 1; index <=5; index++) {
+//     let stars=""
+//     for (let i = 1; i <=index ; i++) {
+//         stars+="$"
+//     }
+//     console.log(stars);
+// }
+
+// took help from bard ...
+// for (let i = 1; i <= 5; i++) { // Loop 5 times
+//     let stars = "";
+//     for (let j = 1; j <= i; j++) { // Loop to add stars for each row
+//       stars += "*";
+//     }
+//     console.log(stars);
+//   }
+
+// control loop
+  
+// task 8 
+
+// for (let index = 1; index <=10; index++) {
+// //  index==5?console.log("*"): console.log(index);  // testing XDXD
+// if (index==5) {
+//     continue
+    
+// }else{console.log(index);}
+// }
+
+// task 9 
+// for (let index = 1; index <=10; index++) {
+// if (index==7) {
+//     break
+    
+// }else{console.log(index);}
+// }
+
