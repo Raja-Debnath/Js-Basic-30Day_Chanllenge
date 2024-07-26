e864c912852e32c91d663ec197145f506e47181b
Raja Debnath <piratesking2110@gmail.com>
2024-07-27 02:42:12 +0530

    Add ES6 features and object manipulation examples

---
+++ es6.js
@@ -0,0 +1,30 @@
+const Name = "raja"
+const Gen = "M"
+const Age = 18
+
+// task 1,2
+// console.log(`My name is ${Name}, im ${Age} year old `);
+
+
+// Array destructuring tsk 3
+const numbers = [10, 20, 30, 40, 50];
+
+// const [firstNumber, secondNumber] = numbers;
+const fst = numbers[3]
+
+// console.log(fst); // Output: 10
+// console.log(snd); // Output: 20
+
+// task 4 
+const book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925
+  };
+  
+const {title,author}=book
+const year = book.year  
+//   console.log(title); // Output: The Great Gatsby
+//   console.log(author); // Output: F. Scott Fitzgerald
+//   console.log(year);
+  
--- objs.js
+++ objs.js
@@ -1,72 +1,72 @@
 const books = [
-    {
-      title: "The Lord of the Rings",
-      author: "J.R.R. Tolkien",
-      genre: "Fantasy",
-      published: 1954,
-    },
-    {
-      title: "To Kill a Mockingbird",
-      author: "Harper Lee",
-      genre: "Fiction",
-      published: 1960,
-    },
-    {
-      title: "Pride and Prejudice",
-      author: "Jane Austen",
-      genre: "Romance",
-      published: 1813,
-    },
-    {
-      title: "1984",
-      author: "George Orwell",
-      genre: "Dystopian",
-      published: 1949,
-    },
-    {
-      title: "The Catcher in the Rye",
-      author: "J.D. Salinger",
-      genre: "Coming-of-age",
-      published: 1951,
-    },
-    {
-      title: "The Great Gatsby",
-      author: "F. Scott Fitzgerald",
-      genre: "Classic",
-      published: 1925,
-    },
-    {
-      title: "Harry Potter and the Sorcerer's Stone",
-      author: "J.K. Rowling",
-      genre: "Fantasy",
-      published: 1997,
-    },
-    {
-      title: "The Alchemist",
-      author: "Paulo Coelho",
-      genre: "Inspirational",
-      published: 1988,
-    },
-    {
-      title: "Sapiens: A Brief History of Humankind",
-      author: "Yuval Noah Harari",
-      genre: "Nonfiction",
-      published: 2011,
-    },
-    {
-      title: "The Hitchhiker's Guide to the Galaxy",
-      author: "Douglas Adams",
-      genre: "Science Fiction",
-      published: 1979,
-    },
-  ]
+  {
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    published: 1954,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Fiction",
+    published: 1960,
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    genre: "Romance",
+    published: 1813,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian",
+    published: 1949,
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    genre: "Coming-of-age",
+    published: 1951,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Classic",
+    published: 1925,
+  },
+];
 
-//   task 1 
+const key = Symbol(10);
+
+const look = {
+  book: "maths",
+  author: "R.D. Sharma",
+  date: "2024",
+  key: 55,
+};
+
+//   task 1
 // console.log(books.title,books.author,books.published);
 // console.log(books[0].author);
 // books.forEach((books)=> console.log(books.title,books.author,books.published))
 
-// task 2 
+// task 2
 // books.forEach((books)=> console.log(books.title,books.author,))
 
-// task 3 
+// task 3
+
+// const search =()=>{ return console.log(look.author)}
+// search()
+
+// task 4
+// let year = "1960";
+// // books.forEach((i,year) => i.published==year?console.log(i.published):"false");
+// books.filter((i,year="1960")=> {if(i.published==year){
+//   console.log(i.published);
+
+// }})
+
+
+// task 5 
+
